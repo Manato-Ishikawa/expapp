1a19c019a8b860619aaf2abc3e6e86f5543953d7
Ichiyanagi Atsushi <c0a20017b3@edu.teu.ac.jp>
2021-12-30 14:40:58 +0900

    ichiyanagi

--- app.js
+++ app.js
@@ -4,3 +4,32 @@ const port = 3000;
 
 app.get('/', (req, res) => res.send('Ishikawa!'));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+app.get("/weapons", (req,res) => {
+    const products = [
+    {
+      id: 1,
+      name: "sword",
+    },
+    {
+      id: 2,
+      name: "spear",
+    },
+    ,
+    {
+      id: 3,
+      name: "axe",
+    },
+    {
+      id: 4,
+      name: "bow",
+    },
+    {
+      id: 5,
+      name: "rapier",
+    },
+
+   ];
+  
+   res.json(products);
+  });
